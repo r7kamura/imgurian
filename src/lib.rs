3fed466606268458e6de45eae35220202f2002e3
Ryo Nakamura <r7kamura@gmail.com>
2021-11-10 22:13:34 +0900

    Map reqwest's JSON error into debuggable error (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,26 @@ pub mod request_builders;
 
 #[derive(Debug)]
 pub enum Error {
-    ImgurError { details: models::Error },
-    ReqwestError { source: reqwest::Error },
+    ImgurError {
+        details: models::Error,
+    },
+    JsonError {
+        source: serde_path_to_error::Error<serde_json::Error>,
+    },
+    ReqwestError {
+        source: reqwest::Error,
+    },
 }
 
 impl From<reqwest::Error> for Error {
     fn from(source: reqwest::Error) -> Self {
         Error::ReqwestError { source }
+    }
+}
+
+impl From<serde_path_to_error::Error<serde_json::Error>> for Error {
+    fn from(source: serde_path_to_error::Error<serde_json::Error>) -> Self {
+        Error::JsonError { source }
     }
 }
 
