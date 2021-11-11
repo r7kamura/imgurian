bd98a1597e5bbca7a73905de912cb75c0909fcf4
Ryo Nakamura <r7kamura@gmail.com>
2021-11-11 21:09:27 +0900

    Some utility types into crate::types module (#11)

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,5 @@ pub mod client;
 pub mod models;
 pub mod request_builders;
 
-#[derive(Debug)]
-pub enum Error {
-    ImgurError {
-        details: models::Error,
-    },
-    JsonError {
-        source: serde_path_to_error::Error<serde_json::Error>,
-    },
-    ReqwestError {
-        source: reqwest::Error,
-    },
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(source: reqwest::Error) -> Self {
-        Error::ReqwestError { source }
-    }
-}
-
-impl From<serde_path_to_error::Error<serde_json::Error>> for Error {
-    fn from(source: serde_path_to_error::Error<serde_json::Error>) -> Self {
-        Error::JsonError { source }
-    }
-}
-
-pub type Result<T, E = Error> = std::result::Result<T, E>;
+mod types;
+pub use types::{Error, Result};
