da7902af78e16a52e987d2b2463c3ff60503c0c2
Ryo Nakamura <r7kamura@gmail.com>
2021-11-12 01:48:21 +0900

    Add Client::update_image (#13)

---
+++ examples/update_image.rs
@@ -0,0 +1,18 @@
+use imguria::client::Client;
+use imguria::error::Error;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let client_id =
+        env::var("IMGUR_CLIENT_ID").expect("Specify IMGUR_CLIENT_ID environment variable.");
+    let client = Client::builder().client_id(client_id).build()?;
+    let basic = client
+        .update_image("5LRukel6PzSi5H3")
+        .title("test2")
+        .description("test2")
+        .send()
+        .await?;
+    dbg!(basic);
+    Ok(())
+}
--- src/request_builders/mod.rs
+++ src/request_builders/mod.rs
@@ -2,10 +2,12 @@ mod delete_image;
 mod favorite_image;
 mod get_account;
 mod get_image;
+mod update_image;
 mod upload_image;
 
 pub use delete_image::*;
 pub use favorite_image::*;
 pub use get_account::*;
 pub use get_image::*;
+pub use update_image::*;
 pub use upload_image::*;
